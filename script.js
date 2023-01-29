ce92e6fe47bd8be7485108877bf2f2e11fca57d4
Hearthfire <rasikhmuhammad@gmail.com>
2023-01-30 01:03:20 +0530

    add operate function

--- script.js
+++ script.js
@@ -16,3 +16,30 @@ const divide = function(a,b) {
     return a/b;
 }
 
+//called if user performs action on two numbers
+function operate(a,b,operator) {
+
+    //check if a and b are numbers
+
+    if(typeof(a) !== 'number' && typeof(b) !== "number") {
+        return "ERROR, please enter a valid number";
+    } 
+
+    //call the appropriate function based on operator chosen by user
+    switch(operator) {
+        case '+':
+            return add(a,b);
+            break;
+        case '-':
+            return subtract(a,b);
+            break;
+        case 'x':
+            return multiply(a,b);
+            break;
+        case '/':
+            return divide(a,b);
+            break;
+        default: 
+            return "ERROR, operator not valid!";
+    }
+}
