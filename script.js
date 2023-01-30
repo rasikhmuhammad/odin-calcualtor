acde7b60874c726f052b69cd921784e057d5ab3b
Hearthfire <rasikhmuhammad@gmail.com>
2023-01-30 06:53:38 +0530

    added ability to solve complex expressions, without using equal to key

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
-//Global variables
-let operand1 = "";
+//data variables
+let operand1 = "0";
 let operand2 = "";
 let operator = "";
 
 //DOM element for calculator display
 const display = document.querySelector('div.display');
+
+
+//function to update display
+const updateDisplay = function (content) {
+    display.textContent = content;
+}
+
+//function to reset data variables
+const reset = function () {
+    operand1 = "0";
+    operand2 = "";
+    operator = "";
+}
+
+//function to clear display
+const clearDisplay = function() {
+    display.classList.remove("error");
+    reset();
+    display.textContent = "0";
+}
+
+//function to display error message
+const displayError = function(error) {
+    display.textContent = error;
+    display.classList.add("error");
+    reset();
+}
 
 
 const add = function (a,b) {
@@ -20,8 +47,10 @@ const multiply = function(a,b) {
 }
 
 const divide = function(a,b) {
-    if(b === 0) return "ERROR, cannot divide by 0";
-
+    if(b === 0) {
+        displayError("ERROR, cannot divide by 0");
+        return "0";
+    }
     return a/b;
 }
 
@@ -31,7 +60,8 @@ function operate(a,b,operator) {
     //check if a and b are numbers
 
     if(typeof(a) !== 'number' && typeof(b) !== "number") {
-        return "ERROR, please enter a valid number";
+         displayError("ERROR, please enter a valid number");
+         return "0";
     } 
 
     //call the appropriate function based on operator chosen by user
@@ -48,25 +78,8 @@ function operate(a,b,operator) {
         case "\u00F7":
             return divide(a,b);
             break;
-        default: 
-            return "ERROR, operator not valid!";
     }
 }
-
-
-//function to update display
-const updateDisplay = function (content) {
-    display.textContent = content;
-}
-
-//function to clear display
-const clearDisplay = function() {
-    operand1 = "";
-    operand2 = "";
-    operator = "";
-    display.textContent = "";
-}
-
 
 //main function
 function calculate() {
@@ -77,22 +90,35 @@ function calculate() {
     //clear calculator
     clearKey.addEventListener('click', clearDisplay);
 
+    //event listener on number keys
     const inputKeys = document.querySelectorAll(".show-input");
     inputKeys.forEach(key => key.addEventListener('click', (e) => {
         if(operator !== "") {
             operand2 += e.target.textContent;
             updateDisplay(operand2);
         } else {
-            operand1 += e.target.textContent;
+            if(operand1 === "0" || operand1 === ""){
+                operand1 = e.target.textContent;
+            } else {
+                operand1 += e.target.textContent;
+            }
             updateDisplay(operand1);
-        }      
+        }
     }));
 
+    //event listener on operator keys
     const operatorKeys = document.querySelectorAll(".operator");
     operatorKeys.forEach( key => key.addEventListener( 'click',(e) => {
-        operator = e.target.textContent;  
+        if (operand1 !== "" && operand2 !== "" && operator !== "") {
+            operand1 = operate(Number(operand1), Number(operand2), operator);
+            updateDisplay(operand1);
+            operator = "";
+            operand2 = "";
+        } 
+        operator = e.target.textContent;
     }));
 
+    //event listener on equal to key
     const equalKey = document.querySelector("#equal-key");
     equalKey.addEventListener('click', (e) => {
         if(operand1 !== "" && operand2 !== "" && operator !== "") {
@@ -103,7 +129,7 @@ function calculate() {
         }
 
         else {
-            display.textContent = "ERROR, please provide proper input";
+            displayError("ERROR, please provide both operands");
             return "ERROR, please provide proper input";
         }
     });
